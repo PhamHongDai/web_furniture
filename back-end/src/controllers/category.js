cfc2dcd4348a17e1592b994cc9766ac8432a755f
thienloc662001 <thienloc662001@gmail.com>
2022-12-13 22:02:23 +0700

    update category

--- back-end/src/controllers/category.js
+++ back-end/src/controllers/category.js
@@ -1,99 +1,99 @@
-const { Category } = require("../models");
+const {Category, Product} = require("../models");
 const slugify = require("slugify");
 const shortid = require("shortid");
 
 const createCategories = (categories, parentId = null) => {
-  const categoryList = [];
-  let category;
-  if (parentId == null) {
-    category = categories.filter((cat) => cat.parentId == undefined);
-  } else {
-    category = categories.filter((cat) => cat.parentId == parentId);
-  }
-  for (let cat of category) {
-    categoryList.push({
-      _id: cat._id,
-      name: cat.name,
-      slug: cat.slug,
-      parentId: cat.parentId,
-      categoryImage: cat.categoryImage,
-      children: createCategories(categories, cat._id),
-    });
-  }
-  return categoryList;
+    const categoryList = [];
+    let category;
+    if (parentId == null) {
+        category = categories.filter((cat) => cat.parentId == undefined);
+    } else {
+        category = categories.filter((cat) => cat.parentId == parentId);
+    }
+    for (let cat of category) {
+        categoryList.push({
+            _id: cat._id,
+            name: cat.name,
+            slug: cat.slug,
+            parentId: cat.parentId,
+            categoryImage: cat.categoryImage,
+            children: createCategories(categories, cat._id),
+        });
+    }
+    return categoryList;
 };
 
 exports.addCategory = (req, res) => {
-  const { name, parentId } = req.body;
-  const categoryObj = {
-    name,
-    slug: `${slugify(name)}-${shortid.generate()}`,
-  };
+    const {name, parentId} = req.body;
+    const categoryObj = {
+        name,
+        slug: `${slugify(name)}-${shortid.generate()}`,
+    };
 
-  if (req.file) {
-    categoryObj.categoryImage = req.file.path;
-  }
-  const cate = new Category(categoryObj);
-  cate.save((error, category) => {
-    if (error) {
-      return res.status(400).json({ error });
+    if (req.file) {
+        categoryObj.categoryImage = req.file.path;
     }
-    if (category) {
-      return res.status(201).json({ category });
-    }
-  });
+    const cate = new Category(categoryObj);
+    cate.save((error, category) => {
+        if (error) {
+            return res.status(400).json({error});
+        }
+        if (category) {
+            return res.status(201).json({category});
+        }
+    });
 };
 
+async function getCategory(res, status = 200) {
+    Category.find({isDisabled: {$ne: true}}).exec((error, categories) => {
+        if (error) {
+            return res.status(400).json({error});
+        } else {
+            const categoriesList = createCategories(categories);
+            return res.status(200).json({categories: categoriesList});
+        }
+    });
+}
+
 exports.getCategories = (req, res) => {
-  Category.find({ isDisabled: { $ne: true } }).exec((error, categories) => {
-    if (error) {
-      return res.status(400).json({ error });
-    } else {
-      const categoriesList = createCategories(categories);
-      return res.status(200).json({ categories: categoriesList });
-    }
-  });
+    getCategory(res);
 };
 
 exports.getFlatCategories = (req, res) => {
-  Category.find({}).exec((error, categories) => {
-    if (error) {
-      return res.status(400).json({ error });
-    } else {
-      return res.status(200).json({ categories });
-    }
-  });
+    Category.find({}).exec((error, categories) => {
+        if (error) {
+            return res.status(400).json({error});
+        } else {
+            return res.status(200).json({categories});
+        }
+    });
 };
 
 exports.setDisableCategory = async (req, res) => {
-  const { ids } = req.body.payload;
-  const deletedCategories = [];
-  for (let i = 0; i < ids.length; i++) {
-    const deleteCategory = await Category.findOneAndUpdate(
-      {
-        _id: ids[i]._id,
-      },
-      { isDisabled: true }
-    );
-    deletedCategories.push(deleteCategory);
-  }
-  if (deletedCategories.length == ids.length) {
-    res.status(201).json({ message: "Categories removed" });
-  } else {
-    res.status(400).json({ message: "Something went wrong" });
-  }
+    const {_id} = req.body;
+    console.log("hello")
+    try {
+        const category = await Category.findOneAndUpdate({_id}, {isDisabled: true});
+        if (category) {
+            res.status(200).json({message: "Disabled category successfully"});
+        } else {
+            res.status(400).json({error: "no found product"});
+        }
+    } catch (error) {
+        res.status(400).json({error});
+    }
 };
 
 exports.updateCategories = async (req, res) => {
-  const { _id, name} = req.body;
-  const category = {
-    name,
-  };
-  if (req.file) {
-    category.categoryImage = req.file.path;
-  }
-  const newCategory = await Category.findOneAndUpdate({ _id }, category, {
-    new: true,
-  });
-  res.status(202).json({ category: newCategory });
+    const { _id, name} = req.body;
+    const category = {
+        name,
+    };
+    if (req.file) {
+        category.categoryImage = req.file.path;
+    }
+    const newCategory = await Category.findOneAndUpdate({ _id }, category, {
+        new: true,
+    });
+    res.status(202).json({ category: newCategory });
 };
